afcb30d5892bcb6669ddf60c273f30b33b0874e9
Erik Vroon <erik_vroon@hotmail.com>
2020-04-25 14:44:45 +0200

    Implemented hold control to move entire stack

---
+++ Extension/Patches/PartyCharacterVmPatches.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+using YAPO.Global;
+
+namespace YAPO.Patches
+{
+    public class PartyCharacterVmPatches
+    {
+        [HarmonyPatch(typeof(PartyCharacterVM), "ApplyTransfer")]
+        public class ApplyTransferPatch
+        {
+            public static void Prefix(PartyCharacterVM __instance, ref int transferAmount)
+            {
+                if (States.HotkeyControl) transferAmount = __instance.Troop.Number;
+            }
+        }
+    }
+}
