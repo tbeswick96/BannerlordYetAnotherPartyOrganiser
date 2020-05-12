baf370c610b75486d160485fd518c43da796c353
Tim Beswick <contact.tim.here@gmail.com>
2020-05-12 11:44:35 +0100

    Fix autosave and new save
    - If save is not found, load loaded configuration and save as new
    
    (cherry picked from commit cf349e485b52527d33591fb93242d57eaf0ffd9a)

--- Extension/Configuration/Patches/MBSaveLoadPatches.cs
+++ Extension/Configuration/Patches/MBSaveLoadPatches.cs
@@ -16,7 +16,7 @@ namespace YAPO.Configuration.Patches
         {
             public static void Postfix(string saveName)
             {
-                States.CurrentSaveName = saveName;
+                States.LoadedSaveName = saveName;
                 Task unused = Task.Run(LoadAsync);
             }
 
@@ -24,7 +24,7 @@ namespace YAPO.Configuration.Patches
             {
                 SorterConfigurationManager.Instance.LoadConfigurations();
                 (States.PartySorterConfiguration, States.OtherSorterConfiguration) =
-                    SorterConfigurationManager.Instance.GetConfiguration(States.CurrentSaveName);
+                    SorterConfigurationManager.Instance.GetConfiguration(States.LoadedSaveName);
             }
         }
 
@@ -35,7 +35,7 @@ namespace YAPO.Configuration.Patches
             {
                 // Removing ".tmp" from the savename is necessary as MBB starts saving the file as <name>.tmp,
                 // and only removes the ".tmp" when it finalises the save file
-                States.CurrentSaveName = saveName.Replace(".tmp", "");
+                States.NewSaveName = saveName.Replace(".tmp", "");
                 Task unused = Task.Run(SaveAsync);
             }
 
